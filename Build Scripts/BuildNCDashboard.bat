a8367f4304dc244b46f3361126fc111e338799ab
Michael Costanzo <mvcostanzo@outlook.com>
2020-08-19 19:45:27 -0400

    Automated Git Update

--- Build Scripts/BuildNCDashboard.bat
+++ Build Scripts/BuildNCDashboard.bat
@@ -1,5 +1,6 @@
 cd "C:\Michael_Docs\Git Repositories\covid-tracking-data"
 curl --url https://api.covidtracking.com/v1/states/daily.csv --output "C:\Michael_Docs\Git Repositories\covid-tracking-data\data\daily.csv"
+git add "C:\Michael_Docs\Git Repositories\covid-tracking-data\data\daily.csv"
 git commit -a --file="C:/Michael_Docs/Git Repositories/covid-tracking-data/.git/COMMITMESSAGE"
 
 git fetch --progress --verbose upstream
