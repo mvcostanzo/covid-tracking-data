c7a044f1a5e7d4c459102aaf8298b193d167da39
Michael Costanzo <mvcostanzo@outlook.com>
2020-08-19 19:41:57 -0400

    Update to NY COVID Data

--- Build Scripts/BuildNCDashboard.bat
+++ Build Scripts/BuildNCDashboard.bat
@@ -1,4 +1,7 @@
 cd "C:\Michael_Docs\Git Repositories\covid-tracking-data"
+curl --url https://api.covidtracking.com/v1/states/daily.csv --output "C:\Michael_Docs\Git Repositories\covid-tracking-data\data\daily.csv"
+git commit -a --file="C:/Michael_Docs/Git Repositories/NY Times COVID Data/.git/COMMITMESSAGE"
+
 git fetch --progress --verbose upstream
 git merge upstream/master
 git push --progress --verbose origin refs/heads/master:refs/heads/master --tags
