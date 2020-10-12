2dd84b24d2bb07207f661da2ead90d2f7b97951a
Michael Costanzo <mvcostanzo@outlook.com>
2020-10-12 19:29:53 -0400

    Changed the Build Script to Stop Pulling from Upstream

--- Build Scripts/BuildNCDashboard.bat
+++ Build Scripts/BuildNCDashboard.bat
@@ -1,9 +1,5 @@
-cd "C:\Michael_Docs\Git Repositories\covid-tracking-data"
-git pull
 curl --url https://api.covidtracking.com/v1/states/daily.csv --output "C:\Michael_Docs\Git Repositories\covid-tracking-data\data\daily.csv"
 git add "C:\Michael_Docs\Git Repositories\covid-tracking-data\data\daily.csv"
 git commit -a --file="C:/Michael_Docs/Git Repositories/covid-tracking-data/.git/COMMITMESSAGE"
 
-git fetch --progress --verbose upstream
-git merge upstream/master
 git push --progress --verbose origin refs/heads/master:refs/heads/master --tags
